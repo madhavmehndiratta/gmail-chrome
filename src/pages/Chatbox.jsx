83053dc796911946a478069429a16ed1de1b1a03
madhavmehndiratta <madhavmehndiratta2k3@gmail.com>
2024-11-30 23:16:33 +0530

    frontend fix

--- src/pages/Chatbox.jsx
+++ src/pages/Chatbox.jsx
@@ -1,20 +1,38 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import { z } from "zod";
 import "./Chatbot.css";
+import Loading from "../components/Loading";
 
 const Chatbot = () => {
   const [response, setResponse] = useState("");
   const [inputValue, setInputValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const inputSchema = z
+    .string()
+    .min(2, "Input must be at least 2 characters long");
 
   async function getResponse() {
+    try {
+      inputSchema.parse(inputValue);
+      setError("");
+    } catch (e) {
+      setError(e.errors[0].message);
+      return;
+    }
+
+    setResponse("");
+    setIsLoading(true);
     let s = await ai.languageModel.create({
       systemPrompt: "You are a friendly conversation bot.",
     });
 
     const response = await s.prompt(
-      "Reply to the user with max 300 words in a friendly manner. Return the answer in markdown. Here is the question: " +
+      "Remember the whole conversation.. Reply to the user with max 300 words in a friendly manner. Return the answer in markdown. Here is the question: " +
         inputValue,
     );
+    setIsLoading(false);
     setResponse(response.trim());
   }
 
@@ -23,7 +41,7 @@ const Chatbot = () => {
       <div className="chatbotHead">Ask Mimo</div>
       <div className="ques-segment">
         <img className="img-mimo" src="/images/mimo.png" alt="Mimo Avatar" />
-        <p className="mimo">How can I help you today?</p>
+        <div className="mimo">How can I help you today?</div>
       </div>
       <div className="userInput">
         <input
@@ -32,10 +50,14 @@ const Chatbot = () => {
           onChange={(e) => setInputValue(e.target.value)}
           value={inputValue}
         />
+        {error && <p className="error-message">{error}</p>}
       </div>
       <button className="submitButton" onClick={getResponse}>
         Submit
       </button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {isLoading ? <Loading /> : ""}
+      </div>
       <div className="Output">
         <ReactMarkdown>{response}</ReactMarkdown>
       </div>
